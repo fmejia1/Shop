4958557112e65e85758a6bcac4930534b571c5e2
francisco <mejia.francisco@gmail.com>
2019-06-26 08:07:16 -0600

    add Products table and controller

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿
+/*esta clase es importante ya que aqui se agregan todas las tablas que se necesitan
+ connectar con la db*/
+namespace Shop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;//para usar con la DbContext si no lanzara un error.
+    using Shop.Web.Data.Entities;//para que la tabla Product pueda ser usada.
+    using System;
+    using System;
+    
+    //esta clase hereda de DbContext
+    public class DataContext : DbContext
+    {
+        /*se crea una propiedad de tipo DbSet para cada tabla que deseamos
+         que se incluya en la bd, osea esta tabla se va a mapear en el entity framework
+         con esto ya los productos seran tratados como colecciones de un objeto de tipo Productos y no
+         como campos de una tabla, cada vez que necesitenmos un campo de la tabla accesaremos
+         a travez de la propiedad de tipo DbSet<Product>*/
+        public DbSet<Product> Products { get; set; }
+        //para conectarme a la bd creo este constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        /*NOTA==>no le hemos dicho a que bd nos vamos a conectar, hay que marcar el string de la _
+         * conexion de la bd y eso lo hacemos en el archivo json llamado appsettings.json */
+    }
+}
---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,39 @@
+﻿namespace Shop.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    //esta clase se convertira mas adelante en base de datos
+    public class Product
+    {
+        public int Id { get; set; }//este atributo sera autoincrementable en la bd
+
+        public string Name { get; set; }//nombre del producto
+
+        /*a esto se le conoce como decoradores y no es mas que formatear en la base de datos
+         la forma en que se va a ver el campo Price donde "{0:C2}" donde la moneda tendra un
+         formato de currency 2 y ademas de eso tendra la moneda configurada en la pc o sea
+         aparecera la separacion de miles por una coma y con dos decimales.*/
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }//precio del producto formateado para mostrarlo como moneda
+
+        [Display(Name = "Image")]//asi se desplegara el atributo ImageUrl
+        public string ImageUrl { get; set; }//aqui ira la ruta de la imagen
+
+        [Display(Name = "Last Purchase")]//asi se desplegara el atributo LastPurchase
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]//asi se desplegara el atributo LastSale
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Availabe?")]//asi se desplegara el atributo IsAvailabe
+        public bool IsAvailabe { get; set; }
+
+        /*aqui se mostrar solo el numero con la coma de miles y para editar el campo solo double
+         por eso ApplyFormatInEditMode es igual a false solo mostrara los numeros sin coma*/
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+
+  
+}
